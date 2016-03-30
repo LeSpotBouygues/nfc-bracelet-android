82f5484feda1ddcd5a0c974aed5bd0dfd9619924
tristan_moreau <trst.moreau@gmail.com>
2016-03-30 20:06:57 +0200

    add listeners on companion list

--- app/src/main/java/com/example/tristan/nfcbracelet/adapters/CompanionAdapter.java
+++ app/src/main/java/com/example/tristan/nfcbracelet/adapters/CompanionAdapter.java
@@ -1,13 +1,19 @@
 package com.example.tristan.nfcbracelet.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.tristan.nfcbracelet.R;
+import com.example.tristan.nfcbracelet.activities.CompanionActivity;
 import com.example.tristan.nfcbracelet.database.CompanionDB;
 import com.example.tristan.nfcbracelet.models.Companion;
 
@@ -17,6 +23,8 @@ import java.util.ArrayList;
  * Created by Tristan on 30/03/2016.
  */
 public class CompanionAdapter extends ArrayAdapter<Companion> {
+    private static final String TAG = "CompanionAdapter";
+
     public CompanionAdapter(Context context, ArrayList<Companion> companions) {
         super(context, 0, companions);
     }
@@ -24,15 +32,37 @@ public class CompanionAdapter extends ArrayAdapter<Companion> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
-        Companion companion = getItem(position);
+        final Companion companion = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_companion, parent, false);
         }
         // Lookup view for data population
-        TextView companionName = (TextView) convertView.findViewById(R.id.companionName);
+        final TextView companionName = (TextView) convertView.findViewById(R.id.companionName);
+        final CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.checkBox);
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.deleteButton);
+
         // Populate the data into the template view using the data object
         companionName.setText(companion.getFirstName() + " " + companion.getLastName());
+        companionName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBox.isChecked()) {
+                    Intent intent = new Intent(getContext(), CompanionActivity.class);
+                    getContext().startActivity(intent);
+                } else {
+                    Toast.makeText(getContext(), companionName.getText() + "'s presence hasn't been checked", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                remove(companion);
+            }
+        });
+
+
         // Return the completed view to render on screen
         return convertView;
     }
--- app/src/main/java/com/example/tristan/nfcbracelet/fragments/CompanionsFragment.java
+++ app/src/main/java/com/example/tristan/nfcbracelet/fragments/CompanionsFragment.java
@@ -15,6 +15,7 @@ import android.widget.ListView;
 
 import com.example.tristan.nfcbracelet.R;
 import com.example.tristan.nfcbracelet.activities.CompanionActivity;
+import com.example.tristan.nfcbracelet.activities.HomeActivity;
 import com.example.tristan.nfcbracelet.adapters.CompanionAdapter;
 import com.example.tristan.nfcbracelet.models.Companion;
 import com.example.tristan.nfcbracelet.models.Data;
@@ -33,6 +34,8 @@ public class CompanionsFragment extends Fragment {
     private static final String TAG = "CompanionsFragment";
 
     private Data mData;
+    private CompanionAdapter adapter;
+    private ListView listView;
 
     public CompanionsFragment() {
         // Required empty public constructor
@@ -51,22 +54,10 @@ public class CompanionsFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_companions,
                 container, false);
-        final CompanionAdapter adapter = new CompanionAdapter(getContext(), mData.getTeam().getCompanions());
+        adapter = new CompanionAdapter(getContext(), mData.getTeam().getCompanions());
         // Attach the adapter to a ListView
-        ListView listView = (ListView) rootView.findViewById(R.id.companionsList);
+        listView = (ListView) rootView.findViewById(R.id.companionsList);
         listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Companion companion = adapter.getItem(position);
-                Log.d(TAG, "Click on " + companion.getFirstName());
-                CheckBox checkBox = (CheckBox) adapter.getView(position, view.findViewById(R.id.checkBox), parent);
-                if (checkBox.isChecked()) {
-                    Intent intent = new Intent(getActivity(), CompanionActivity.class);
-                    startActivity(intent);
-                }
-            }
-        });
 
         return rootView;
     }
